074df8f3cc88ade487b300c8dc9ec50bcd51fa3a
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-03-15 19:57:57 -0700

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,19 @@
+//start of function assignment code
+function firstFunction() { //function assignment
+    var g = "Grilled"; 
+    var c= "Cheese";
+    var s = "Grilled"+ " " +"Cheese"
+    document.getElementById("snack").innerHTML=s;
+    
+}
+//end of function assignment code
+
+document.write('<br/>');
+
+//start of operator function assignment code
+function operatorFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this course!"; //"+=" will concatenate
+    document.getElementById("concatenate").innerHTML=sentence;
+}
+//end of operator function assignment code
