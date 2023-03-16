cd76ae83c6f3bd6bd927ff7233fbb7203e38e0fc
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-03-15 18:52:45 -0700

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,44 @@
+window.alert("Hello World!"); //window alert to create a pop up and button when going to the page
+
+document.write("Hello World!"); //document write to print to the page
+
+document.write('<br/>');
+
+var A = "String"; //printing via variable
+document.write(A);
+
+var Z = "JavaScript Intro"; //String Challenge
+window.alert(Z);
+
+document.write('<br/>');
+
+document.write("The girl\'s cat said, \"meow\"."); //escaping characters
+
+document.write('<br/>');
+
+document.write("The girl\'s dog said,"
++ " \"ruff\".");
+
+document.write('<br/>');
+
+
+var B = "The animals" + " are noisy."; //Concatenated Challenge
+document.write(B);
+
+document.write('<br/>');
+
+
+var Cast = "Last of Us", Joel = "Pedro Pascal", //Multiple Variables Challenge
+Ellie="Bella Ramsey", Marlene="Merle Dandridge";
+document.write(Joel);
+
+
+document.write('<br/>');
+
+document.write(5+5); //Expression Assignment
+
+document.write('<br/>');
+
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string."
+document.write(Sent1+Sent2)
