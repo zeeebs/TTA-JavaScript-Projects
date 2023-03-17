ea7500c8c83b0d836aeae4294bb92aad214b7748
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-03-16 17:18:43 -0700

    Function Challenge

---
+++ Challenges/JS/function.js
@@ -0,0 +1,7 @@
+//function
+function functionChallenge(x1, x2) {
+    return x1+x2;
+}
+//document.getElementByID() method
+document.getElementById('calc').innerHTML
+    = functionChallenge(10, 5);
