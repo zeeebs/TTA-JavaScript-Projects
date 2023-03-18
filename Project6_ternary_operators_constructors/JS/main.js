cbcdaf6fb8fea30ccc840c0de5d5e12a0caed72f
Zella Stewart <125938574+zeeebs@users.noreply.github.com>
2023-03-17 18:52:04 -0700

    project5 fix

---
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,13 @@
+< !DOCTYPE html >
+    <html>
+
+        <head>
+            <h3></h3>
+            <script src="JS/main.js"></script>
+        </head>
+
+        <body>
+
+        </body>
+
+    </html>
